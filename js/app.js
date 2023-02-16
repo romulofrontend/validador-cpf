9721c5fd3137f487d94cc7a809c7dba4ac7a818f
Romulo Rosa <romulo.frontend@gmail.com>
2023-02-16 16:39:08 -0300

    js msgNumbers esconder

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
-//Capturando elementos no dom
 var cpfField = document.getElementById("cpf-field");  
 var msgNumbers = document.getElementById("msg-only-numbers");
 var msgSuccess = document.getElementById("success");
 var msgError = document.getElementById("error");
+
+
+msgNumbers.style.display = "none";
