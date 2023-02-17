ee05d328f24652c0e62a8b3ae7259ee55a40abb5
Romulo Rosa <romulo.frontend@gmail.com>
2023-02-17 11:11:32 -0300

    js separando funcoes

--- js/app.js
+++ js/app.js
@@ -1,33 +1,34 @@
-var cpfField = document.getElementById("cpf-field");  
 var msgNumbers = document.getElementById("msg-only-numbers");
 var msgSuccess = document.getElementById("success");
 var msgError = document.getElementById("error");
 
-
 msgNumbers.style.display = "none";
 
-//funcao chamada ao clicarno botao validar
-function validacao(){  
-  
-  var cpfFieldValue = cpfField.value;
 
+function validaCPF(cpf){
   function onlyNumbers(str) {
     return /^\d+$/.test(str);
   }
   
-  if(onlyNumbers(cpfFieldValue) != true) {    
+  if(onlyNumbers(cpf) != true) {    
     msgNumbers.style.display = "block";
     msgError.style.display = "block";
     msgSuccess.style.display = "none";
   } else {
     msgNumbers.style.display = "none";
-    if(cpfFieldValue.length != 11) {   
+    if(cpf.length != 11) {   
       msgError.style.display = "block";
       msgSuccess.style.display = "none"; 
     } else {
       msgError.style.display = "none";   
       msgSuccess.style.display = "block";   
     }
-  }  
-  
+  }
+}
+
+
+function validacao(){  
+  var cpf = document.getElementById("cpf-field").value;
+
+  var resultadoValidacao = validaCPF(cpf);   
 }
